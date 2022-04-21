e79b7a55de8bebba75c6af7a385b911fb61a41dd
bearagones <baragones@mail.sfsu.edu>
2022-04-21 16:22:27 -0700

    Created Vertex.java

---
+++ src/main/Vertex.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+
+public class Vertex {
+    private final int label;
+    private Vertex parent;
+    private final LinkedList<Edge> edgeList;
+
+    //Each vertex will have an edgeList associated with it
+    public Vertex(int label) {
+        this.label = label;
+        this.edgeList = new LinkedList<>();
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public Vertex getParent() {
+        return parent;
+    }
+
+    public int getLabel() {
+        return label;
+    }
+
+    public LinkedList<Edge> getEdgeList() {
+        return edgeList;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(label);
+    }
+}
