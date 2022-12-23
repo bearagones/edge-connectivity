bab4eb73de5852dcb1a4b8f99adb940a885c8aca
bearagones <baragones@mail.sfsu.edu>
2022-12-22 17:13:01 -0800

    deleted unused methods

--- src/main/Vertex.java
+++ src/main/Vertex.java
@@ -2,20 +2,11 @@ import java.util.LinkedList;
 
 public class Vertex {
     private final int label;
-    private Vertex parent;
     private final LinkedList<Edge> edgeList;
 
-    //Each vertex will have an edgeList associated with it
     public Vertex(int label) {
         this.label = label;
         this.edgeList = new LinkedList<>();
-    }
-
-    public Vertex getParent() {
-        return parent;
-    }
-    public void setParent(Vertex parent) {
-        this.parent = parent;
     }
 
     public int getLabel() {
